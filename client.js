f21d0b8abd1efa62512332433b664680cdb7b48b
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-09-23 19:36:02 +0000

    added client and play files for snek

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  return conn;
+}
+
+module.exports = {
+  connect
+}; 
